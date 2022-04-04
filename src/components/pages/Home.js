309237f08e36d0c778f7c639bbc4a4da45da3e45
chambalain <chigbochiemelie@gmail.com>
2022-04-04 20:26:03 +0100

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,23 @@
+import React  from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards';
+
+function Home(){
+
+return (
+
+<>
+
+<HeroSection /> 
+
+ <Cards /> 
+
+</>
+
+
+);
+
+}
+
+export default Home;
